7eea8bef9f51bc12eac68ef85bf4a5b3be1cb186
jackynguyen110 <jackynguyen110@gmail.com>
2016-03-17 16:59:23 +0700

    base source

---
+++ FormInputDemo/Preference.swift
@@ -0,0 +1,13 @@
+//
+//  Preference.swift
+//  FormInputDemo
+//
+//  Created by DEREK DO on 17/3/16.
+//  Copyright © 2016 jackyCode.com. All rights reserved.
+//
+
+import Foundation
+
+class Preferences {
+    var autoRefresh = false, playSounds = true, showPhotos = true
+}
